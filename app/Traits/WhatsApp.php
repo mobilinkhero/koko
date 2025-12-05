c07153afd987ce01005963dd35d74966aee9686e
Usman Ali <usman@eventswally.com>
2025-12-06 01:11:52 +0500

    Fixed contact form validation and footer links

--- app/Traits/WhatsApp.php
+++ app/Traits/WhatsApp.php
@@ -2471,7 +2471,6 @@ trait WhatsApp
                 'reply_text' => $aiResponseText,
                 'bot_header' => '',
                 'bot_footer' => '',
-                'tenant_id' => $this->wa_tenant_id ?? tenant_id() ?? null,
             ];
 
             $this->logToAiFile($logFile, "SENDING MESSAGE TO USER:");
